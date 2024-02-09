02e413df0dc63447a5c3f7ada4570940e4e30cda
Abdallah Aloush <abdallahaloush6@gmail.com>
2024-02-09 21:53:14 +0200

    filename from stdin

--- src/file.c
+++ src/file.c
@@ -2,15 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include "contact.h"
 
 #define MAX_LINE_LENGTH 512     //TODO: optimize this length
+#define MAX_PATH_LENGTH 512
 
 bool load_file(){
-    FILE* save_file = fopen("d.txt", "r");
+    char filename[MAX_PATH_LENGTH];
+    safe_input(filename, MAX_PATH_LENGTH, "\033[34mEnter the name of the file you want to load from:\033[0m\t");
+
+    FILE* save_file = fopen(filename, "r");
     if (save_file == NULL){
         printf("\033[1;31merror: File does not exist\033[0m\n");
         return false;
     }
+
     int i = 0;
     char buffer[MAX_LINE_LENGTH];
     while(fgets(buffer, MAX_LINE_LENGTH, save_file)){
